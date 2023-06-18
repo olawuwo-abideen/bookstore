1e46e58e45dbc303b35cea45504612d86fa42044
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-06-18 17:04:38 +0100

    models

---
+++ models/store.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+
+const StoreSchema = new mongoose.Schema(
+    {
+
+    }
+
+)
+
+
+
+
+
+
+
+
+
+
+module.exports = mongoose.model('Store', StoreSchema);
