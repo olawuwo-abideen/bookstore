f07bb8a11c92e0c15bbe4d8fe771ae556978da3e
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-06-19 21:28:11 +0100

    models

--- models/authors.js
+++ models/authors.js
@@ -4,8 +4,27 @@ const mongoose = require('mongoose');
 
 const AuthorSchema = new mongoose.Schema(
     {
-
-    }
+        name: {
+          type: String,
+          required: [true, 'Please provide your name'],
+          maxlength: 50,
+        },
+        description: {
+          type: String,
+          required: [true, 'Please provide the description'],
+          maxlength: 100,
+        },
+        books: {
+          type: String,
+          required: [true, 'Please provide the book title'],
+          maxlength: 100,
+        },
+        website: {
+            type: String,
+            required: [true, 'Please provide the link'],
+            maxlength: 100,
+          },
+      },
 
 )
 
--- models/books.js
+++ models/books.js
@@ -4,9 +4,47 @@ const mongoose = require('mongoose');
 
 const BookSchema = new mongoose.Schema(
     {
-
-    }
-
+        title: {
+          type: String,
+          required: [true, 'Please provide the book title'],
+          maxlength: 50,
+        },
+        authors: {
+          type: String,
+          required: [true, 'Please provide the author name'],
+          maxlength: 50,
+        },
+        ISBNCode: {
+          type: String,
+          required: [true, 'Please provide the ISBN number'],
+          maxlength: 30,
+        },
+        store: {
+            type: String,
+            required: [true, 'Please provide the store name'],
+            maxlength: 100,
+          },
+          genre: {
+            type: String,
+            required: [true, 'Please provide the genre of the book'],
+            maxlength: 100,
+          },
+         description: {
+            type: String,
+            required: [true, 'Please provide the book description'],
+            maxlength: 100,
+          },
+          review: {
+            type: String,
+            required: [true, 'Please provide the book review'],
+            maxlength: 100,
+          },
+          price: {
+            type: String,
+            required: [true, 'Please provide the book price'],
+            maxlength: 100,
+          },
+      },
 )
 
 
