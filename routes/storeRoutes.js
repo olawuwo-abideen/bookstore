886c4efc454adff142d5dc8b8da9f0f4e6450b8e
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-06-22 21:38:34 +0100

    store

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -2,30 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 
+const {
+    getAllStores,
+    createStores,
+    getStore,
+    getStoreBooks,
+    getStoreEmployees,
+    getStoreBooksales,
+    updateStores
+    
+    } = require('../controllers/store')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+router.route('/stores').get(getAllStores).post(createStores)
+router.route('/stores/:id').get(getStore).patch(updateStores)
+router.route('/stores/:id/books').get(getStoreBooks)  
+router.route('/stores/:id/employees').get(getStoreEmployees)  
+router.route('/stores/:id/booksales').get(getStoreBooksales)  
 
 
 
