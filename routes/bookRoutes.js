ca515839a53769d23fba1029ce07fcd5495b1be2
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-06-22 23:59:05 +0100

    fix

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -7,14 +7,14 @@ const {
     createBook,
     getBook,
     updateBook,
-    getAllBooksAuthors,
-    getAllBooksReviews
+    getBooksAuthors,
+    getBooksReviews
     } = require('../controllers/books')
 
 router.route('/books').get(getAllBooks).post(createBook)
 router.route('/books/:id').get(getBook).patch(updateBook)
-router.route('/books/:id/authors').get(getAllBooksAuthors)
-router.route('/books/:id/reviews').get(getAllBooksReviews)
+router.route('/books/:id/authors').get(getBooksAuthors)
+router.route('/books/:id/reviews').get(getBooksReviews)
 
 
 
