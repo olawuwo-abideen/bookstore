0e4cce794f7f9d4ae2d2acb8d9b9d5e5839350bd
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-06-22 21:05:03 +0100

    author

--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -5,7 +5,19 @@ const router = express.Router();
 
 
 
+const {
+    getAllBooks,
+    createBook,
+    getBook,
+    updateBook,
+    getAllBooksAuthors,
+    getAllBooksReviews
+    } = require('../controllers/books')
 
+router.route('/books').get(getAllBooks).post(createBook)
+router.route('/books/:id').get(getBook).patch(updateBook)
+router.route('/books/:id/authors').get(getAllBooksAuthors)
+router.route('/books/:id/reviews').get(getAllBooksReviews)
 
 
 
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -6,6 +6,17 @@ const router = express.Router();
 
 
 
+const {
+    getAllBooks,
+    createBook,
+    getBook,
+    updateBook,
+    deleteBook ,
+   
+    } = require('../controllers/books')
+
+router.route('/').get(getAllBooks).post(createBook)
+router.route('/:id').get(getBook).patch(updateBook).delete(deleteBook)
 
 
 
