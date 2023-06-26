50dfc0cd211d5d7b0ccd98d696a0b3d3c1fdbae3
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-06-26 09:12:05 +0100

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
-
+const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // database
 const connectDB = require('./db/connect');
+const app = express();
+
+//  routers
+const  authorRouter = require('./routes/authorRoutes')
+const  bookRouter = require('./routes/bookRoutes')
+const  booksaleRouter = require('./routes/clientRoutes')
+const  clientreviewRouter = require('./routes/clientreviewRoutes')
+const  employeeRouter = require('./routes/employeeRoutes')
+const  storeRouter = require('./routes/storeRoutes')
+
+
+// midddlewares
+app.use(notFound)
+app.use(errorHandlerMiddleware)   
+
+
+
+app.use('/api/v1/author', authorRouter);
+app.use('/api/v1/book', bookRouter);
+app.use('/api/v1/booksale', booksaleRouter);
+app.use('/api/v1/clientreview', clientreviewRouter);
+app.use('/api/v1/employee', employeeRouter);
+app.use('/api/v1/store', storeRouter);
+
 
 
 
