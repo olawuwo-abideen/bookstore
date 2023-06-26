589bbb9bbee71b3a00e03bf3fb2f3e52217b0b5b
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-06-26 12:40:19 +0100

    controllers

--- controllers/books.js
+++ controllers/books.js
@@ -21,15 +21,26 @@ const  createBook = asyncWrapper(async (req, res) => {
 
 
 const getBook = asyncWrapper(async (req, res) => {
-    const tasks = await Task.find({})
-    res.status(200).json({tasks})   
+    const {id:bookID} = req.params
+    const book = await Task.findOne({_id:bookID});
+    if(!book){
+        return next(createCustomError (`No book with id : ${bookID}`, 404))
+    }
+res.status(200).json({book})  
 });
 
 
 
 const updateBook = asyncWrapper(async (req, res) => {
-    const tasks = await Task.find({})
-    res.status(200).json({tasks})   
+    const {id: bookID} = req.params
+    const book = await Book.findOneAndUpdate({_id: bookID}, req.body,{
+        new: true,
+        runValidators: true 
+});
+    if(!book) {
+        return next(createCustomError (`No task with id : ${bookID}`, 404))
+    }
+    res.status(200).json({book}) 
 });
 
 
@@ -41,8 +52,7 @@ const getBooksAuthors = asyncWrapper(async (req, res) => {
 
 
 
-const     getBooksReviews
-= asyncWrapper(async (req, res) => {
+const     getBooksReviews = asyncWrapper(async (req, res) => {
     const tasks = await Task.find({})
     res.status(200).json({tasks})   
 });
